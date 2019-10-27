9869c28415220561fa592bfdd0ab03203c50654b
Seungho Kim <niceb5y@gmail.com>
2019-10-28 00:00:57 +0900

    Bump version Update layout

--- components/navbar.js
+++ components/navbar.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default () => (
   <nav className="navbar navbar-light my-4 px-0">
-    <Link href="/" className="navbar-brand mr-auto text-primary">
-      <a>Share</a>
+    <Link href="/">
+      <a className="navbar-brand mr-auto text-primary">Share</a>
     </Link>
   </nav>
 )
